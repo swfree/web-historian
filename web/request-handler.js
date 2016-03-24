8fbcdf97d6af374012fe53c7fd11164a4fad6ba3
swfree <skye.free@gmail.com>
2016-03-23 17:09:44 -0700

    Remove pomander

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,58 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var qs = require('querystring');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === 'GET' && req.url === '/') {
+    var indexPath = archive.paths.siteAssets + '/index.html';
+    fs.readFile(indexPath, 'utf8', function(err, data){
+      handleError(res, err);
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    });
+  }
+  
+  else if (req.url === '/styles.css') {
+    var stylePath = archive.paths.siteAssets + '/styles.css';
+    fs.readFile(stylePath, 'utf8', function(err, data){
+      handleError(res, err);
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.end(data);
+    });
+  }
+
+  else {
+    fs.readFile(archive.paths.archivedSites + req.url, 'utf8', function(err, data) {
+      handleError(res, err);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(data);
+    })
+  }
+
+  if (req.method === 'POST' && req.url === '/') {
+    var postContents = '';
+    req.setEncoding('utf8');
+    req.on('data', function(chunk) {
+      postContents += chunk;
+    });
+    req.on('end', function() {
+      var postedURL = qs.parse(postContents).url + '\n'
+      fs.appendFile(archive.paths.list, postedURL, function(err) {
+        handleError(res, err);
+        res.writeHead(302);
+        res.end();
+      });
+    });
+  }
 };
+
+var handleError = function(res, err) {
+  if(err) {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('' + err);
+  }
+}
+
+// GET '/www.google.com'
